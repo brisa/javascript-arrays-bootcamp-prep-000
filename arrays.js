caabf6dc362795406ab2dde92c211c42e45c49e4
Brisa Carter <brisa.carter@gmail.com>
2019-05-13 17:36:46 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ['snickers', 'hundred', 'grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, anElement){
+  var newArr = [anElement, ...arr]
+  return newArr
+
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, anElement){
+  arr.unshift(anElement)
+  return arr
+}
+
+function addElementToEndOfArray(arr, anElement){
+var newArr = [...arr, anElement]
+  return newArr
+}
+function destructivelyAddElementToEndOfArray(arr, anElement){
+arr.push(anElement)
+  return arr
+
+}
+function accessElementInArray(arr, index){
+   return arr[2]
+}
+function removeElementFromBeginningOfArray(arr){
+  arr.shift()
+  return array
+}
+function removeElementFromEndOfArray(arr){
+  arr.pop()
+  return array
+}
