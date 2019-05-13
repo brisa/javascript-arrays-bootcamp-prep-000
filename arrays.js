c578eee2437279065d146f259cbe2bf06e5281d4
Brisa Carter <brisa.carter@gmail.com>
2019-05-13 18:02:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function removeElementFromBeginningOfArray(arr){
 }
 function removeElementFromEndOfArray(arr){
   var newArr = arr.pop()
-  return arr
+  return newArr
 }
 function destructivelyRemoveElementFromBeginningOfArray(arr){
   arr.shift()
